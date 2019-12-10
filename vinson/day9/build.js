978736cc46cef360d5aee3da4d7e94d4c5affd14
Vinson Fernandez <vinsonfernandez@gmail.com>
2019-12-09 20:32:12 -0800

    time

--- vinson/day9/build.js
+++ vinson/day9/build.js
@@ -1,8 +1,13 @@
-const webPage = [{
+const webPage = [
+    {
     query: 'body',
     type: 'h1',
     text: 'amazing'
-}, {
+},     {
+        query: 'body',
+        type: 'div',
+        text: 'profile here'
+    },{
         query: 'body',
         type: 'ul',
 }, {
@@ -12,16 +17,30 @@ const webPage = [{
     }
 ];
 
+let selected = null;
+
 webPage.forEach( el => {
     createElements3(el);
 });
 
 users.forEach( function (userObj) {
    console.log('username', userObj);
-    createElements3({
+    const li = createElements3({
         query: 'ul',
         type: 'li',
         text: userLi(userObj),
+    });
+    li.addEventListener('click', function () {
+        if (selected) {
+            selected.classList.remove('selected');
+        }
+        selected = this;
+        const name = this.querySelector('h3').innerText;
+        this.classList.add('selected');
+        const user = users.filter( function (item) {
+            return item.name === name;
+        })[0];
+        document.querySelector('div').innerHTML = profileHtml(user);
     })
 });
 
@@ -38,28 +57,26 @@ function userLi(user) {
     `;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function createElements2(type, query, text) {
-    const el = document.createElement(type);
-    document.querySelector(query).appendChild(el);
-    if (text) {
-        el.innerText = text;
-    }
+function profileHtml(user) {
+return  `
+<h2> ${user.name},</h2>
+<img height="300" src="${user.avatar}" alt="">
+<ul>
+<li>
+    age: ${user.age},
+</li>
+<li>
+      job: ${user.job},
+</li>
+<li>
+      vip: ${user.vip},
+</li>
+<li>
+status: ${user.status}
+</li>
+</ul>
+      
+        `
 }
 
 function createElements3( {type, query, text } ) {
@@ -68,4 +85,5 @@ function createElements3( {type, query, text } ) {
     if (text) {
         el.innerHTML = text;
     }
+    return el;
 }
